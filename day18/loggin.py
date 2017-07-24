4cd8baa20f4fea7f8c634aa56be4907f42c40a89
JuliaJia <juliajia25191@gmail.com>
2017-07-24 10:28:17 +0800

    test

---
+++ day18/loggin.py
@@ -0,0 +1,40 @@
+import logging
+
+
+
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+#                     datefmt='%a,%d %b %Y %H:%M:%S',
+#                     # )
+#                     filename='test.log',
+#                     filemode='w')
+#
+# # 五个日志的级别
+# logging.debug('debug message')
+# logging.info('info message')
+# logging.warning('warning message')
+# logging.error('error message')
+# logging.critical('critical message')
+
+
+#step1:创建logger对象
+logger = logging.getLogger()   #拿一个logger对象
+#step2:创建日志Handler
+fh = logging.FileHandler('test.log') #创建一个handler，用于写入日志文件
+ch = logging.StreamHandler()   #创建一个handler，用于将日志打到屏幕上
+#step3:定义日志格式并应用到handler中
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') #定义日志格式
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+#step4:添加Handler到logger对象中
+logger.addHandler(fh) #logger对象可以添加多个fh和ch对象
+logger.addHandler(ch)
+
+#step5：设置日志信息
+logger.debug('logger debug message')
+logger.info('logger info message')
+logger.warning('logger warning message')
+logger.error('logger error message')
+logger.critical('logger critical message')
